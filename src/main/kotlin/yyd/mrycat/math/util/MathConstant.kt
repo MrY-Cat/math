b5fbebb8402bbaf274706d541538334cd71a6b39
Y-y-d <2278010681@qq.com>
2023-06-21 17:56:07 +0800

    新增高精度自然常数

---
+++ src/main/kotlin/yyd/mrycat/math/util/MathConstant.kt
@@ -0,0 +1,16 @@
+@file:Suppress("unUsed", "FunctionName")
+
+package yyd.mrycat.math.util
+
+import java.math.BigDecimal
+
+/**
+ * 用于存储一些数学常数.
+ */
+object MathConstant
+{
+    /**
+     * 自然常数，精确到128位小数.
+     */
+    val E = BigDecimal("2.71828182845904523536028747135266249775724709369995957496696762772407663035354759457138217852516642742746639193200305992181741359")
+}
