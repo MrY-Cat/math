0e90ec3950d30d85f07d04da02653397ec0a6a07
Y-y-d <2278010681@qq.com>
2023-06-28 11:34:03 +0800

    .

--- src/main/kotlin/yyd/mrycat/math/test/Tests.kt
+++ src/main/kotlin/yyd/mrycat/math/test/Tests.kt
@@ -30,7 +30,7 @@ fun test13(name:String = "集齐完备事件组期望次数公式")
     println("----测试[$name]开始----")
     //val eachProbability = arrayOf(1.0/6, 2.0/6, 3.0/6).toDoubleArray()//奇怪硬币默认项：7.29999
     //val eachProbability = arrayOf(1.0/6, 2.0/6, 1.5/6, 1.5/6).toDoubleArray()//默认项：9.13809
-    val eachProbability = arrayOf(1.0/20, 7.0/20, 3.0/20, 9.0/20).toDoubleArray()//默认项：
+    val eachProbability = arrayOf(1.0/20, 7.0/20, 3.0/20, 9.0/20).toDoubleArray()//默认项：21.907399
     val EX = gatherEventClusterEX(*eachProbability)
     println(EX)
     for(n in 2..50)
