7f94d9f6dc03e2373a93688061b06b61a4082f22
Y-y-d <2278010681@qq.com>
2023-06-28 10:45:05 +0800

    image.

--- src/main/kotlin/yyd/mrycat/math/problem/GatherEventCluster.kt
+++ src/main/kotlin/yyd/mrycat/math/problem/GatherEventCluster.kt
@@ -13,10 +13,13 @@ import kotlin.math.absoluteValue
 /**
  * 集齐完备事件组期望次数.
  *
- * 即计算对N（无需给出，即[probabilities]中元素数）个事件构成的完备事件组，使每个事件都恰好至少发生一次所需的总试验次数X的期望E(X).
+ * 该算法采用基于期望公式和拆分矩阵的方法，计算对N（无需给出，即[probabilities]中元素数）个事件构成的完备事件组，使每个事件都恰好至少发生一次所需的总试验次数X的期望E(X).
+ *
+ * 若要查看该算法的公式，请访问[公式][https://github.com/MrY-Cat/math/tree/main/image/gatherEventClusterEX.jpg].
  * @param probabilities 每个事件的概率(顺序不影响结果).
  * @param InfiniteSeriesCalculationAccuracy 当存在某个概率≠1/N时，算法中无穷级数计算的项数，不指定时默认为10/[probabilities].[min]，注意：项数过少可能导致结果与解析解有较大误差.
- * @exception MathIllegalException 当某个概率不属于0..1或概率总和不属于(1-1E-15)..(1+1E-15)时.
+ * @throws MathIllegalException 当某个概率不属于0..1或概率总和不属于(1-1E-15)..(1+1E-15)时.
+ * @see gatherEventClusterEY
  */
 fun gatherEventClusterEX(vararg probabilities:Double, InfiniteSeriesCalculationAccuracy:Int = (10/probabilities.min()).toInt()):BigDecimal
 {
@@ -58,12 +61,22 @@ fun gatherEventClusterEX(vararg probabilities:Double, InfiniteSeriesCalculationA
     return result
 }
 
-//使用马尔科夫链的计算方式
-fun gatherEventClusterEr2(vararg probabilities:Double):BigDecimal
+/**
+ * 集齐完备事件组期望次数.
+ *
+ * 该算法采用基于马尔科夫链的方法，计算对N（无需给出，即[probabilities]中元素数）个事件构成的完备事件组，使每个事件都恰好至少发生一次所需的总试验次数Y的期望E(Y).
+ * @param probabilities 每个事件的概率(顺序不影响结果).
+ * @throws MathIllegalException 当某个概率不属于0..1或概率总和不属于(1-1E-15)..(1+1E-15)时.
+ * @see gatherEventClusterEX
+ */
+fun gatherEventClusterEY(vararg probabilities:Double):BigDecimal
 {
+    if(probabilities.any { it < 0.0 || it > 1.0 }) throw MathIllegalException("n个事件的概率均应属于0到1")
+    if((1.0-probabilities.sum()).absoluteValue > MathConstant.DoublePlusErrorAccuracy) throw MathIllegalException("n个事件的概率总和应为1.0")
     val n = probabilities.size
-    if(probabilities.any { it < 0.0 || it > 1.0 } || (1.0-probabilities.sum()).absoluteValue > MathConstant.DoublePlusErrorAccuracy) throw MathIllegalException("n个事件的概率均应属于0到1，且总和应为1")
     var result = BigDecimal.ZERO
+
+
     for(s in 0..n-1)//s个独立变量，每个都可能是1-n
     {
         val matrix = Matrix(pow(n.toDouble(), s.toDouble()).toInt(), s) { _, _ -> 0 }
