1a14d61cbf0305be9b14f1d3333f674abfb3c178
Y-y-d <2278010681@qq.com>
2023-12-28 16:36:52 +0800

    尝试生成javadoc.jar，未果

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,28 @@ plugins {
     val kotlinVersion = "1.7.10"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
+    id("org.jetbrains.dokka") version "1.5.30"//javadoc生成插件
 }
 
 group = "yyd.mrycat.math"
 version = "0.1.0-beta.2"
-
-repositories() {
+/*
+tasks.dokkaJavadoc.configure {
+    // 导出的文档目录路径
+    //outputDirectory.set(new File(rootDir, "javadoc"))
+    dokkaSourceSets {
+        named("main") {
+            noAndroidSdkLink.set(true)
+            noStdlibLink.set(true)
+            noJdkLink.set(true)
+        }
+        configureEach {
+            // 包的定义
+            includes.from("yyd.mrycat.math")
+        }
+    }
+}*/
+repositories {
     mavenCentral()
 }
 dependencies {
